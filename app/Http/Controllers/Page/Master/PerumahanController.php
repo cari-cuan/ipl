9a07036fa53238486e5c4669eaa24514c983e6be
candrasw <candra.setiadi@machtwatch.co.id>
2025-06-18 14:28:27 +0700

    [ADD] create perumahan

--- app/Http/Controllers/Page/Master/PerumahanController.php
+++ app/Http/Controllers/Page/Master/PerumahanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Page\Master;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Master\PerumahanModel;
 
 class PerumahanController extends Controller
 {
@@ -20,7 +21,7 @@ class PerumahanController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.master.perumahan.create');
     }
 
     /**
@@ -28,7 +29,31 @@ class PerumahanController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // dd($request->all());
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'phone' => 'required|string|max:20',
+            'report_date' => 'required|numeric',
+            'bank_account_number' => 'required|string|max:50',
+            'bank_name' => 'required|string|max:100',
+            'bank_account_name' => 'required|string|max:100',
+        ]);
+
+        $product = PerumahanModel::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'phone' => $validatedData['phone'],
+            'report_date' => $validatedData['report_date'],
+            'bank_account_number' => $validatedData['bank_account_number'],
+            'bank_name' => $validatedData['bank_name'],
+            'bank_account_name' => $validatedData['bank_account_name'],
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // Redirect atau response
+        return redirect()->route('perumahan')->with('success', 'Perumahan berhasil ditambahkan.');
     }
 
     /**
